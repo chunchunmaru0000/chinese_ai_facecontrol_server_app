41f66c0d68c37c5a03f4835d04dac95e87aff2fc
chunchunmaru0000 <cho227675m@gmail.com>
2025-02-17 12:37:26 +0300

    orx

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace testkit
+﻿using System.IO;
+using System;
+
+namespace testkit
 {
 	partial class Form1
 	{
@@ -28,98 +31,135 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.listView1 = new System.Windows.Forms.ListView();
-			this.connect = new System.Windows.Forms.Button();
-			this.Disconnect = new System.Windows.Forms.Button();
-			this.ClearList = new System.Windows.Forms.Button();
+			this.connectBut = new System.Windows.Forms.Button();
+			this.disconnectBut = new System.Windows.Forms.Button();
+			this.clearList = new System.Windows.Forms.Button();
 			this.textPort = new System.Windows.Forms.TextBox();
 			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.portLabel = new System.Windows.Forms.Label();
+			this.vlcControl = new Vlc.DotNet.Forms.VlcControl();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.vlcControl)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// listView1
 			// 
 			this.listView1.HideSelection = false;
-			this.listView1.Location = new System.Drawing.Point(0, 0);
+			this.listView1.Location = new System.Drawing.Point(0, 512);
 			this.listView1.Name = "listView1";
-			this.listView1.Size = new System.Drawing.Size(992, 352);
+			this.listView1.Size = new System.Drawing.Size(768, 160);
 			this.listView1.TabIndex = 0;
 			this.listView1.UseCompatibleStateImageBehavior = false;
 			this.listView1.View = System.Windows.Forms.View.Details;
 			// 
-			// connect
+			// connectBut
 			// 
-			this.connect.Location = new System.Drawing.Point(256, 384);
-			this.connect.Name = "connect";
-			this.connect.Size = new System.Drawing.Size(160, 32);
-			this.connect.TabIndex = 1;
-			this.connect.Text = "connect";
-			this.connect.UseVisualStyleBackColor = true;
-			this.connect.Click += new System.EventHandler(this.button1_Click);
+			this.connectBut.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.connectBut.Location = new System.Drawing.Point(32, 32);
+			this.connectBut.Name = "connectBut";
+			this.connectBut.Size = new System.Drawing.Size(192, 32);
+			this.connectBut.TabIndex = 1;
+			this.connectBut.Text = "Подключиться";
+			this.connectBut.UseVisualStyleBackColor = true;
+			this.connectBut.Click += new System.EventHandler(this.button1_Click);
 			// 
-			// Disconnect
+			// disconnectBut
 			// 
-			this.Disconnect.Location = new System.Drawing.Point(256, 416);
-			this.Disconnect.Name = "Disconnect";
-			this.Disconnect.Size = new System.Drawing.Size(160, 32);
-			this.Disconnect.TabIndex = 2;
-			this.Disconnect.Text = "disconnect";
-			this.Disconnect.UseVisualStyleBackColor = true;
-			this.Disconnect.Click += new System.EventHandler(this.Disconnect_Click);
+			this.disconnectBut.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.disconnectBut.Location = new System.Drawing.Point(32, 64);
+			this.disconnectBut.Name = "disconnectBut";
+			this.disconnectBut.Size = new System.Drawing.Size(192, 32);
+			this.disconnectBut.TabIndex = 2;
+			this.disconnectBut.Text = "Отключиться";
+			this.disconnectBut.UseVisualStyleBackColor = true;
+			this.disconnectBut.Click += new System.EventHandler(this.Disconnect_Click);
 			// 
-			// ClearList
+			// clearList
 			// 
-			this.ClearList.Location = new System.Drawing.Point(256, 448);
-			this.ClearList.Name = "ClearList";
-			this.ClearList.Size = new System.Drawing.Size(160, 32);
-			this.ClearList.TabIndex = 3;
-			this.ClearList.Text = "ClearList";
-			this.ClearList.UseVisualStyleBackColor = true;
-			this.ClearList.Click += new System.EventHandler(this.clearList_Click);
+			this.clearList.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.clearList.Location = new System.Drawing.Point(32, 96);
+			this.clearList.Name = "clearList";
+			this.clearList.Size = new System.Drawing.Size(192, 32);
+			this.clearList.TabIndex = 3;
+			this.clearList.Text = "Очистить список";
+			this.clearList.UseVisualStyleBackColor = true;
+			this.clearList.Click += new System.EventHandler(this.clearList_Click);
 			// 
 			// textPort
 			// 
 			this.textPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.textPort.Location = new System.Drawing.Point(256, 352);
+			this.textPort.Location = new System.Drawing.Point(128, 0);
 			this.textPort.Name = "textPort";
-			this.textPort.Size = new System.Drawing.Size(160, 31);
+			this.textPort.Size = new System.Drawing.Size(95, 31);
 			this.textPort.TabIndex = 4;
 			// 
-			// richTextBox1
+			// panel1
 			// 
-			this.richTextBox1.Location = new System.Drawing.Point(448, 352);
-			this.richTextBox1.Name = "richTextBox1";
-			this.richTextBox1.Size = new System.Drawing.Size(544, 160);
-			this.richTextBox1.TabIndex = 5;
-			this.richTextBox1.Text = "";
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Controls.Add(this.portLabel);
+			this.panel1.Controls.Add(this.disconnectBut);
+			this.panel1.Controls.Add(this.connectBut);
+			this.panel1.Controls.Add(this.clearList);
+			this.panel1.Controls.Add(this.textPort);
+			this.panel1.Location = new System.Drawing.Point(1056, 512);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(256, 160);
+			this.panel1.TabIndex = 6;
+			// 
+			// portLabel
+			// 
+			this.portLabel.AutoSize = true;
+			this.portLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.portLabel.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+			this.portLabel.Location = new System.Drawing.Point(32, 0);
+			this.portLabel.Name = "portLabel";
+			this.portLabel.Size = new System.Drawing.Size(76, 31);
+			this.portLabel.TabIndex = 5;
+			this.portLabel.Text = "Порт";
+			// 
+			// vlcControl
+			// 
+			this.vlcControl.BackColor = System.Drawing.Color.Black;
+			this.vlcControl.Location = new System.Drawing.Point(0, 0);
+			this.vlcControl.Name = "vlcControl";
+			this.vlcControl.Size = new System.Drawing.Size(288, 224);
+			this.vlcControl.Spu = -1;
+			this.vlcControl.TabIndex = 7;
+			this.vlcControl.Text = "vlcControl1";
+			this.vlcControl.VlcLibDirectory = new DirectoryInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "libvlc\\win-x86")); 
+			this.vlcControl.VlcMediaplayerOptions = null;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(992, 516);
-			this.Controls.Add(this.richTextBox1);
-			this.Controls.Add(this.textPort);
-			this.Controls.Add(this.ClearList);
-			this.Controls.Add(this.Disconnect);
-			this.Controls.Add(this.connect);
+			this.ClientSize = new System.Drawing.Size(1324, 707);
+			this.Controls.Add(this.vlcControl);
+			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.listView1);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.vlcControl)).EndInit();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.ListView listView1;
-		private System.Windows.Forms.Button connect;
-		private System.Windows.Forms.Button Disconnect;
-		private System.Windows.Forms.Button ClearList;
+		private System.Windows.Forms.Button connectBut;
+		private System.Windows.Forms.Button disconnectBut;
+		private System.Windows.Forms.Button clearList;
 		private System.Windows.Forms.TextBox textPort;
 		private System.ComponentModel.BackgroundWorker backgroundWorker1;
-		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label portLabel;
+		private Vlc.DotNet.Forms.VlcControl vlcControl;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,59 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.AxHost;
-using FPCLOCK_SVRLib;
 using AxFPCLOCK_SVRLib;
-using System.Net.Http.Headers;
 using System.IO;
 using System.Runtime.InteropServices;
+using DirectShowLib;
+using System.Threading;
 
 namespace testkit
 {
 	public partial class Form1 : Form
 	{
+		string webcamUrl;
+
 		string stateStr = @"
         AAEAAAD/////AQAAAAAAAAAMAgAAAFdTeXN0ZW0uV2luZG93cy5Gb3JtcywgVmVyc2lvbj00LjAuMC4w
         LCBDdWx0dXJlPW5ldXRyYWwsIFB1YmxpY0tleVRva2VuPWI3N2E1YzU2MTkzNGUwODkFAQAAACFTeXN0
         ZW0uV2luZG93cy5Gb3Jtcy5BeEhvc3QrU3RhdGUBAAAABERhdGEHAgIAAAAJAwAAAA8DAAAAJQAAAAIB
         AAAAAQAAAAAAAAAAAAAAABAAAAAAAAEAVgoAADgEAAAAAAAACw==
 ";
-		private AxFPCLOCK_Svr axFPCLOCK_Svr1;
+		private AxFPCLOCK_Svr axFPCLOCK_Svr;
 		private int nIndex = 0;
 		private int Recordnumber = 0;
+
+		private Dictionary<int, string> actions = new Dictionary<int, string>() 
+		{
+			{ 0, "Closed" },   { 1, "Opened" },
+			{ 2, "HandOpen" }, { 3, "ProcOpen" },
+			{ 4, "ProcClose" }, { 5, "IllegalOpen" },
+			{ 6, "IlleagalRemove" }, { 7, "Alarm" },
+			{ 8, "--" }
+		};
 
 		public Form1()
 		{
 			InitializeComponent();
+			//this.vlcControl.VlcLibDirectory = new DirectoryInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "libvlc\\win-x86"));
 
+			InitaxFPCLOCK_Svr();
+			AdjustSomeControls();
+			InitListView();
+
+			//InitUSBWEB();
+		}
+
+		private void InitUSBWEB()
+		{
+			vlcControl.Log += (sender, args) => Console.WriteLine(args.Message);
+
+			webcamUrl = GetWebCamUrl();
+
+			string[] options = new string[]
+			{
+				//$":dshow-vdev=\"{webcamUrl}\"",
+				":dshow-vdev=USB2.0 PC CAMERA",
+				":dshow-adev=none",
+				":live-caching=0",
+			};
+			
+
+			vlcControl.Play("dshow://", options);
+			//vlcControl.Play($"dshow://{webcamUrl}", options);
+			//vlcControl.Play(new Uri("C:\\Users\\user\\Desktop\\vlcvideos\\rules.mp4"));
+		}
+
+		private string GetWebCamUrl()
+		{
+			DsDevice[] cams = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
+			string webcam = "";
+			
+			foreach  (DsDevice cam in cams)
+			{
+				if (cam.Name.ToLower().Contains("camera"))
+					//webcam = $"dshow:// :dshow-vdev=\"{cam.Name}\"";
+					webcam = cam.Name;
+				Console.WriteLine($"\n{cam.Name}");
+			}
+			Console.WriteLine(webcam + '\n');
+			return webcam;
+		}
+
+		#region BEGIN_INITS
+		private void InitaxFPCLOCK_Svr()
+		{
 			byte[] serializedData = Convert.FromBase64String(stateStr);
 
-			axFPCLOCK_Svr1 = new AxFPCLOCK_Svr() { Name = "axFPCLOCK_Svr1" };
+			axFPCLOCK_Svr = new AxFPCLOCK_Svr() { Name = "axFPCLOCK_Svr1" };
 			using (MemoryStream ms = new MemoryStream(serializedData))
-				axFPCLOCK_Svr1.OcxState = new AxHost.State(ms, 1, false, null);
-			/*
-			axFPCLOCK_Svr1.OcxState = 
-				((System.Windows.Forms.AxHost.State)
-				(new System.ComponentModel.ComponentResourceManager(typeof(Form1))
-				.GetObject("axFPCLOCK_Svr1.OcxState")));
-*/
-			axFPCLOCK_Svr1.OnReceiveGLogData += 
+				axFPCLOCK_Svr.OcxState = new AxHost.State(ms, 1, false, null);
+
+			axFPCLOCK_Svr.OnReceiveGLogData +=
 				new AxFPCLOCK_SVRLib._DFPCLOCK_SvrEvents_OnReceiveGLogDataEventHandler(
 					axFPCLOCK_Svr1_OnReceiveGLogData);
 
-			Controls.Add(axFPCLOCK_Svr1);
+			Controls.Add(axFPCLOCK_Svr);
+		}
 
-			connect.Enabled = true;
-			Disconnect.Enabled = false;
-
-			textPort.Text = "7005";
-
+		private void InitListView()
+		{
 			listView1.Columns.Add(" ", 40, HorizontalAlignment.Left);          //一步添加
 			listView1.Columns.Add("EnrollNo", 100, HorizontalAlignment.Left);
 			listView1.Columns.Add("VerifyMode", 100, HorizontalAlignment.Left);
@@ -65,23 +109,53 @@ namespace testkit
 			listView1.Columns.Add("SerialNo", 60, HorizontalAlignment.Left);
 		}
 
+		private void AdjustSomeControls()
+		{
+			connectBut.Enabled = true;
+			disconnectBut.Enabled = false;
+
+			textPort.Text = "7005";
+		}
+		#endregion
+		#region BUTS
 		private void button1_Click(object sender, EventArgs e)
 		{
-			String str = this.textPort.Text;
-			int nPort = Convert.ToInt32(str);
-			axFPCLOCK_Svr1.OpenNetwork(nPort);
-			connect.Enabled = false;
-			this.Disconnect.Enabled = true;
+			if (int.TryParse(textPort.Text, out int port))
+			{
+				axFPCLOCK_Svr.OpenNetwork(port);
+				connectBut.Enabled = false;
+				disconnectBut.Enabled = true;
+			}
+			else
+				MessageBox.Show("НЕВРНОЕ ЧИСЛО ДЛЯ ОТКРЫВАЕМОГО ПОРТА");
 		}
 
-		private void axFPCLOCK_Svr1_OnReceiveGLogData(object sender, AxFPCLOCK_SVRLib._DFPCLOCK_SvrEvents_OnReceiveGLogDataEvent e)
+		private void Disconnect_Click(object sender, EventArgs e)
 		{
-			String strKey = Convert.ToString(nIndex + 1);
-			String str = e.anSEnrollNumber.ToString("D8");
+			if (int.TryParse(textPort.Text, out int port))
+			{
+				axFPCLOCK_Svr.CloseNetwork(port);
+				connectBut.Enabled = true;
+				disconnectBut.Enabled = false;
+			}
+			else
+				MessageBox.Show("НЕВРНОЕ ЧИСЛО ДЛЯ ЗАКРЫВАЕМОГО ПОРТА");
+		}
+
+		private void clearList_Click(object sender, EventArgs e)
+		{
+
+			nIndex = 0;
+			listView1.Items.Clear();
+		}
+		#endregion
+		#region AX
+		private void axFPCLOCK_Svr1_OnReceiveGLogData(object sender, _DFPCLOCK_SvrEvents_OnReceiveGLogDataEvent e)
+		{
+			string strKey = Convert.ToString(nIndex + 1);
+			string str = e.anSEnrollNumber.ToString("D8");
 			long dwCardNum1 = 0;
 			double aTemperature;
-
-			richTextBox1.Text += $"{e.anSEnrollNumber}\n";
 
 			if (e.anSEnrollNumber > 0)
 			{
@@ -89,18 +163,18 @@ namespace testkit
 				int[] imagebuff = new int[200 * 1024];
 				bool bRet;
 				IntPtr ptrIndexFacePhoto = Marshal.AllocHGlobal(imagebuff.Length);
-				bRet = this.axFPCLOCK_Svr1.GetLogImageCS(e.linkindex, ref imagelen, ptrIndexFacePhoto);
+				bRet = axFPCLOCK_Svr.GetLogImageCS(e.linkindex, ref imagelen, ptrIndexFacePhoto);
 				if (bRet && imagelen > 0)
 				{
 					byte[] mbytCurEnrollData = new byte[imagelen];
 					Marshal.Copy(ptrIndexFacePhoto, mbytCurEnrollData, 0, imagelen);
-					System.IO.File.WriteAllBytes(e.anSEnrollNumber.ToString() + "_" + e.anLogDate.ToString("yy_MM_dd_HH_mm_ss") + ".jpg", mbytCurEnrollData);
+					File.WriteAllBytes(e.anSEnrollNumber.ToString() + "_" + e.anLogDate.ToString("yy_MM_dd_HH_mm_ss") + ".jpg", mbytCurEnrollData);
 				}
 				Marshal.FreeHGlobal(ptrIndexFacePhoto);
 			}
 
 			//数据更新，UI暂时挂起，直到EndUpdate绘制控件，可以有效避免闪烁并大大提高加载速度
-			this.listView1.BeginUpdate();
+			listView1.BeginUpdate();
 
 			//this.listView1.Focus();
 			ListViewItem lvi = new ListViewItem();
@@ -169,135 +243,32 @@ namespace testkit
 			str = Convert.ToString(e.anSN);
 			lvi.SubItems.Add(str);
 
-			this.listView1.Items.Add(lvi);
+			listView1.Items.Add(lvi);
 			//this.listView1.Items.(5, str);
 
-			this.listView1.Update();
+			listView1.Update();
 
-			this.listView1.EnsureVisible(nIndex);
-			this.listView1.EndUpdate();  //结束数据处理，UI界面一次性绘制。
+			listView1.EnsureVisible(nIndex);
+			listView1.EndUpdate();  //结束数据处理，UI界面一次性绘制。
 
 			int nResult = 1;
 
-			this.axFPCLOCK_Svr1.SendResultandTime(e.linkindex, e.vnDeviceID, e.anSEnrollNumber, nResult);
+			axFPCLOCK_Svr.SendResultandTime(e.linkindex, e.vnDeviceID, e.anSEnrollNumber, nResult);
 
 			nIndex++;
 			if (nIndex > 1000)
 			{
-				this.nIndex = 0;
-				this.listView1.Items.Clear();
+				nIndex = 0;
+				listView1.Items.Clear();
 			}
 
 		}
 
-		private void Disconnect_Click(object sender, EventArgs e)
-		{
-			String str = this.textPort.Text;
-			int nPort = Convert.ToInt32(str);
-			this.axFPCLOCK_Svr1.CloseNetwork(nPort);
+		public string FormString(int nVerify, int nEnrollNum) =>
+			nEnrollNum == 0 ? actions[nVerify % 8] : nVerify.ToString();
 
-			this.connect.Enabled = true;
-			this.Disconnect.Enabled = false;
-		}
-
-		public String FormString(int nVerify, int nEnrollNum)
-		{
-			int nAction = nVerify % 8;
-			if (nEnrollNum == 0)
-			{
-				switch (nAction)
-				{
-					case 0:
-						return "Closed";
-					//break;
-					case 1:
-						return "Opened";
-					// break;
-					case 2:
-						return "HandOpen";
-					// break;
-					case 3:
-						return "ProcOpen";
-					// break;
-					case 4:
-						return "ProcClose";
-					// break;
-					case 5:
-						return "IllegalOpen";
-					//break;
-					case 6:
-						return "IlleagalRemove";
-					//break;
-					case 7:
-						return "Alarm";
-					//break;
-					case 8:
-						return "--";
-						//break;
-				}
-			}
-			else
-			{
-				return nVerify.ToString();
-
-			}
-
-			return "Not my fault";
-		}
-		public String FormStringlong(int nVerify, long nEnrollNum)
-		{
-			int nAction = nVerify % 8;
-			if (nEnrollNum == 0)
-			{
-				switch (nAction)
-				{
-					case 0:
-						return "Closed";
-					//break;
-					case 1:
-						return "Opened";
-					// break;
-					case 2:
-						return "HandOpen";
-					// break;
-					case 3:
-						return "ProcOpen";
-					// break;
-					case 4:
-						return "ProcClose";
-					// break;
-					case 5:
-						return "IllegalOpen";
-					//break;
-					case 6:
-						return "IlleagalRemove";
-					//break;
-					case 7:
-						return "Alarm";
-					//break;
-					case 8:
-						return "--";
-						//break;
-				}
-			}
-			else
-			{
-				return nVerify.ToString();
-
-			}
-
-			return "Not my fault";
-		}
-		private void clearList_Click(object sender, EventArgs e)
-		{
-
-			this.nIndex = 0;
-			this.listView1.Items.Clear();
-		}
-
-		private void listView1_SelectedIndexChanged(object sender, EventArgs e)
-		{
-
-		}
+		public string FormStringlong(int nVerify, long nEnrollNum) =>
+			nEnrollNum == 0 ? actions[nVerify % 8] : nVerify.ToString();
+		#endregion
 	}
 }
